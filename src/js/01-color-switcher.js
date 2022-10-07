a289a0a6257bf395ac5cf4e7078884414bc4516d
Roman Vanchytskyi <owles@i.ua>
2022-10-08 01:19:06 +0300

    task-1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  intervalId = setInterval(setColor, 1000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+}
+
+function stopChangeColor() {
+  clearInterval(intervalId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+function setColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
