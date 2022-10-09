e9a818bd54c7df5dbad9c8e772b1f49a77eea47d
Roman-Vanchytskyi <104678683+Roman-Vanchytskyi@users.noreply.github.com>
2022-10-09 11:17:07 +0000

    Deploying to gh-pages from @ Roman-Vanchytskyi/goit-js-hw-09@20ed3468b8376d84e97ccaca8a721f2ada95cf48 🚀

--- 01-color-switcher.e1f4c9f3.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;function d(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){o=setInterval(d,1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),e.disabled=!0,t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.e1f4c9f3.js.map
